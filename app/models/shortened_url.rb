ef5d8f0070edf235b281ee715ea539f5b9ed4773
Hank Fanchiu <hank@fanchiu.com>
2015-11-11 21:44:01 -0800

    Refactored lazy queries to be clean and stacked

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -41,20 +41,22 @@ class ShortenedUrl < ActiveRecord::Base
   end
 
   def self.create_for_user_and_long_url!(user, long_url)
-    create!(
+    options = {
       submitter_id: user.id,
       long_url: long_url,
       short_url: random_code
-      )
+    }
+
+    create!(options)
   end
 
   def self.prune(n)
-    url_id_hashes = Visit.select(:shortened_url_id).distinct.
-      where('created_at < ?', n.minutes.ago)
+    distinct_url_ids = Visit.select(:shortened_url_id).distinct.
+    expired_url_ids = distinct_url_ids.where('created_at < ?', n.minutes.ago)
 
-    url_id_hashes.each do |hash|
-      url_id = hash[:shortened_url_id]
-      destroy(url_id)
+    expired_url_ids.each do |hash|
+      expired_url_id = hash[:shortened_url_id]
+      destroy(expired_url_id)
     end
   end
 
@@ -67,14 +69,16 @@ class ShortenedUrl < ActiveRecord::Base
   end
 
   def num_recent_uniques
-    visits.select(:visitor_id).distinct.
-      where('created_at > ?', 10.minutes.ago).count
+    unique_visitors = visits.select(:visitor_id).distinct
+    recent_visits = unique_visitors.where('created_at > ?', 10.minutes.ago)
+    recent_visits.count
   end
 
   private
   def recent_submission_count
-    self.class.where(submitter_id: submitter_id).
-      where('created_at > ?', 1.minutes.ago).count
+    submissions = self.class.where(submitter_id: submitter_id)
+    recent_submissions = submissions.where('created_at > ?', 1.minutes.ago)
+    recent_submissions.count
   end
 
   def too_many_recent_submissions
